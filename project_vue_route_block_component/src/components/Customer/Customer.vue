6ac44285526e7824b7ce199db2c7f9ad7564003d
KhairulIslamAzam <khairulisla.azam@gmail.com>
2022-01-05 09:45:13 +0600

    practice

---
+++ project_vue_route_block_component/src/components/Customer/Customer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Customer</h2>
+    
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur esse voluptatum illum fugit velit. Laborum quas reprehenderit, sint deserunt earum laboriosam fuga, aperiam molestiae quae assumenda exercitationem accusamus velit quia? Nobis adipisci fugit ratione fugiat architecto accusamus quisquam nulla unde.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    methods:{
+        navigateToHome(){
+            this.$router.push({name : 'home'})
+        }
+    }
+}
+</script>
---
+++ project_vue_route_block_component/src/components/Customer/CustomerEdit.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Customer Edit</h2>
+        <P>locale: {{$route.query.locale}}</P>
+        <P>value: {{$route.query.q}}</P>
+    </div>
+</template>
